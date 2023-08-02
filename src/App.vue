3405884d7e81843b7de3d931f03a7ca69df9545c
frankxiong <757134184@qq.com>
2023-08-02 20:16:55 +0800

    feature: 基本完成官网框架

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,24 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import {provide, ref} from "vue"
+import {router} from "./router"
+
+const width = document.documentElement.clientWidth
+const asideVisible = ref(width <= 500 ? false : true)
+
+provide("asideVisible", asideVisible)
+router.afterEach(() => {
+  if (width <= 500) {
+    asideVisible.value = false
+  }
+
+})
+
 </script>
 
 <template>
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+    <router-view></router-view>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
 <style scoped>
@@ -21,9 +28,11 @@ import HelloWorld from './components/HelloWorld.vue'
   will-change: filter;
   transition: filter 300ms;
 }
+
 .logo:hover {
   filter: drop-shadow(0 0 2em #646cffaa);
 }
+
 .logo.vue:hover {
   filter: drop-shadow(0 0 2em #42b883aa);
 }
---
+++ src/views/Home.vue
@@ -0,0 +1,64 @@
+<template>
+  <div>
+    <Topnav/>
+    <div class="banner">
+      <h1>咕噜UI</h1>
+      <h2>一个厉害的UI框架</h2>
+      <p class="actions">
+        <a href="https://github">GitHub</a>
+        <router-link to="/doc">开始</router-link>
+      </p>
+    </div>
+  </div>
+</template>
+<script setup lang="ts">
+import Topnav from "../components/Topnav.vue";
+
+
+</script>
+<style lang="scss" scoped>
+.banner {
+  padding: 100px 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  background: lightgreen;
+
+  > .actions {
+    padding: 8px 0;
+
+    a {
+      margin: 0 8px;
+      background: #fff;
+      display: inline-block;
+      $h: 28px;
+      height: $h;
+      line-height: $h;
+      border-radius: $h/2;
+      padding: 0 8px;
+    }
+  }
+}
+
+.topnav {
+  background: pink;
+  display: flex;
+  padding: 16px;
+
+  > .logo {
+    max-width: 6em;
+    margin-right: auto;
+  }
+
+  > .menu {
+    display: flex;
+    white-space: nowrap;
+    flex-wrap: nowrap;
+
+    > li {
+      margin: 0 1em;
+    }
+  }
+}
+</style>
