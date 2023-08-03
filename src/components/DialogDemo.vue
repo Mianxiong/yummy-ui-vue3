4b8244b638521c1587a93da42e605d541c3fa724
frankxiong <757134184@qq.com>
2023-08-03 15:03:57 +0800

    feature: 完成 Dialog 组件

--- src/components/DialogDemo.vue
+++ src/components/DialogDemo.vue
@@ -1,5 +1,32 @@
 <template>
-  <div>
-    Dialog示例
-  </div>
+  <div>Dialog示例</div>
+  <h1>示例1</h1>
+  <Button @click="toggle">toggle</Button>
+  <Dialog v-model:visible="visible" :ok="confirm" :cancel="cancel">
+    <template v-slot:content>
+      <div>hi</div>
+      <div>hi2</div>
+    </template>
+    <template v-slot:title>
+      <strong>加粗的标题</strong>
+    </template>
+  </Dialog>
 </template>
+
+<script lang="ts" setup>
+import Dialog from "../lib/Dialog.vue"
+import Button from "../lib/Button.vue"
+import {ref} from "vue"
+
+const visible = ref(false)
+const toggle = () => {
+  visible.value = !visible.value
+}
+
+const confirm = () => {
+  return false
+}
+const cancel = () => {}
+
+
+</script>
