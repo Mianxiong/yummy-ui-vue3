ae23b3fd82f4ad84d8b8a453deca768d5f30e326
frankxiong <757134184@qq.com>
2023-08-04 15:15:20 +0800

    feature: 完成 Confirm 组件

---
+++ src/lib/Confirm.vue
@@ -0,0 +1,138 @@
+<template>
+  <div class="gulu-confirm" :class="{fade}">
+    <div class="wrapper" :class="{fade}">
+      <div class="header">
+        <h3>{{ title }}</h3>
+        <a @click="cancel" href="JavaScript:;" class="iconfont icon-close-new"></a>
+      </div>
+      <div class="body">
+        <i class="iconfont icon-warning"></i>
+        <span>{{ text }}</span>
+      </div>
+      <div class="footer">
+        <Button @click="cancel" level="normal" size="normal">取消</Button>
+        <Button @click="submit" level="main" size="normal">确认</Button>
+      </div>
+    </div>
+  </div>
+</template>
+<script lang="ts" setup>
+import {onMounted, ref, toRefs} from "vue"
+import Button from "../lib/Button.vue"
+
+interface Props {
+  title?: string,
+  text?: string,
+  cancelCallback?: Function,
+  confirmCallback?: Function
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  title: "温馨提示",
+  type: ""
+})
+
+// 对话框默认关闭
+const fade = ref(false)
+onMounted(() => {
+  // TODO:
+  // 过渡效果需要在元素创建完毕后，延时一会儿加上才会触发过渡效果
+  setTimeout(() => {
+    fade.value = true
+  }, 0)
+})
+// 取消
+const cancel = () => {
+  props.cancelCallback()
+}
+// 确认
+const submit = () => {
+  props.confirmCallback()
+}
+
+
+</script>
+<style scoped lang="scss">
+.gulu-confirm {
+  position: fixed;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  z-index: 8888;
+  background: rgba(0, 0, 0, 0);
+
+  &.fade {
+    transition: all 0.4s;
+    background: rgba(0, 0, 0, 0.5);
+  }
+
+  .wrapper {
+    width: 400px;
+    background: #fff;
+    border-radius: 4px;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -60%);
+    opacity: 0;
+
+    &.fade {
+      transition: all 0.4s;
+      transform: translate(-50%, -50%);
+      opacity: 1;
+    }
+
+    .header,
+    .footer {
+      height: 50px;
+      line-height: 50px;
+      padding: 0 20px;
+    }
+
+    .body {
+      padding: 20px 40px;
+      font-size: 16px;
+
+      .icon-warning {
+        color: #27ba9b;
+        margin-right: 3px;
+        font-size: 16px;
+      }
+    }
+
+    .footer {
+      text-align: center;
+
+      .gulu-button {
+        margin-left: 20px;
+      }
+    }
+
+    .header {
+      position: relative;
+
+      h3 {
+        font-weight: normal;
+        font-size: 18px;
+      }
+
+      a {
+        position: absolute;
+        right: 15px;
+        top: 15px;
+        font-size: 20px;
+        width: 20px;
+        height: 20px;
+        line-height: 20px;
+        text-align: center;
+        color: #999;
+
+        &:hover {
+          color: #666;
+        }
+      }
+    }
+  }
+}
+</style>
--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,8 @@ import "github-markdown-css"
 import {createApp} from "vue"
 import App from "./App.vue"
 // import Markdown from "./components/Markdown.vue"
+import Confirm from "./lib/Confirm.ts"
+import Message from "./lib/Message.ts"
 
 import {router} from "./router"
 
@@ -12,3 +14,5 @@ const app = createApp(App)
 app.use(router)
 app.mount("#app")
 // app.component("Markdown", Markdown)
+app.config.globalProperties.$confirm = Confirm
+app.config.globalProperties.$message = Message
